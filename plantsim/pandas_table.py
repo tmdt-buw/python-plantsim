0d6ff27bbdb314f05e48ccc3b80cd86bcc8907d2
Constantin WdP <cwaubertdepuise@uni-wuppertal.de>
2021-04-14 12:19:29 +0200

    FIX: Fixed PandasTable object to be independent of a loader module. Also changed the buffer file names to resolve potential file name conflicts with

--- plantsim/pandas_table.py
+++ plantsim/pandas_table.py
@@ -1,19 +1,18 @@
 import os
 import pandas as pd
 
-from loader.plantsim_loader import PlantsimLoader
-
 class PandasTable():
 
-    def __init__(self, object_name, table_buffer_path=''):
+    def __init__(self, plantsim, object_name, table_buffer_path=''):
         """
-        Table mapping for PlantSim Tables (e.g., DataTable, ExplorerTable)
+        Pandas Table mapping for PlantSim Tables (e.g., DataTable, ExplorerTable)
         - stores table in a .txt file in the python script directory
         - returns that table as a pandas Dataframe
         :param plantsim: Plantsim instance (with loaded model) that is queried
         :param table_name: The object name within Plantsim relative to the current path context
+        :param table_buffer_path: manually set path of the table_buffer relative to the current path context
         """
-        self.plantsim = PlantsimLoader.get_plantsim()
+        self.plantsim = plantsim
         self._table = None
         self._table_name = object_name
         self._table_buffer_path = os.path.join(table_buffer_path, 'table_buffer')
@@ -26,7 +25,8 @@ class PandasTable():
             os.makedirs(self._table_buffer_path)
 
         abs_table_buffer_path = os.path.abspath(self._table_buffer_path)
-        buffer_file_name = f'{self._table_name}_tool_buffer.txt'
+        path_adjusted_table_name = self._table_name.replace('.', '_').replace(':', '_')
+        buffer_file_name = f'{path_adjusted_table_name}_buffer.txt'
         abs_buffer_file_path = os.path.join(abs_table_buffer_path, buffer_file_name)
 
         # store table from Plant Simulation
